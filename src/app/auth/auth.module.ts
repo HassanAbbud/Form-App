9134abdd3fdf42045c0ecdf905dc96648c2f128c
Hassan Abbud <hassanabbud@hotmail.com>
2024-07-15 12:01:33 -0600

    App routes implementation

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -2,10 +2,19 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { AuthRoutingModule } from './auth-routing.module';
+import { RegisterPageComponent } from './pages/register-page/register-page.component';
+import { BasicPageComponent } from '../reactive/pages/basic-page/basic-page.component';
+import { DynamicPageComponent } from '../reactive/pages/dynamic-page/dynamic-page.component';
+import { SwitchesPageComponent } from '../reactive/pages/switches-page/switches-page.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    RegisterPageComponent,
+    BasicPageComponent,
+    DynamicPageComponent,
+    SwitchesPageComponent
+  ],
   imports: [
     CommonModule,
     AuthRoutingModule
