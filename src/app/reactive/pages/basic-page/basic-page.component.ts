436a1a66723d5d8ef4b8760425e4caab850c2cc1
Hassan Abbud <hassanabbud@hotmail.com>
2024-07-15 16:55:29 -0600

    Validations & showing field errors to user

--- src/app/reactive/pages/basic-page/basic-page.component.ts
+++ src/app/reactive/pages/basic-page/basic-page.component.ts
@@ -1,11 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
+
+const rtx5090 ={
+  name: "RTX 5090",
+  price: 2600,
+  inStock: 6,
+}
 @Component({
   templateUrl: './basic-page.component.html',
   styles: ``
 })
-export class BasicPageComponent {
+export class BasicPageComponent implements OnInit{
 
   public myForm:FormGroup = this.fb.group({
     name: ["", [Validators.required, Validators.minLength(3)]],
@@ -15,8 +21,42 @@ export class BasicPageComponent {
 
   constructor(private fb:FormBuilder){}
 
-  onSave():void {
+  ngOnInit(): void {
+    this.myForm.reset(rtx5090)
+  }
 
+  isNotValidField( field: string ): boolean | null {
+    return this.myForm.controls[field].errors
+      && this.myForm.controls[field].touched;
+  }
+
+  getFieldError( field: string ): string | null {
+
+    if ( !this.myForm.controls[field] ) return null;
+
+    const errors = this.myForm.controls[field].errors || {};
+
+    for (const key of Object.keys(errors) ) {
+      switch( key ) {
+        case 'required':
+          return 'This field is required';
+
+        case 'minlength':
+          return `There must be at least ${ errors['minlength'].requiredLength } characters.`;
+
+        case 'min':
+          return `Value must be greater than ${ errors['min'].min }.`;
+      }
+    }
+
+    return null;
+  }
+
+  onSave():void {
+    if(!this.myForm.valid) {
+      this.myForm.markAllAsTouched();
+      return;
+    }
     console.log(this.myForm.value)
   }
 }
