8252a93f0a62a66ef5d96348871a44e44c2b0d54
Hassan Abbud <hassanabbud@hotmail.com>
2024-07-16 10:56:22 -0600

    Validators service & injectable isNotValidField() function

--- src/app/auth/pages/register-page/register-page.component.ts
+++ src/app/auth/pages/register-page/register-page.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import * as customValidators from '../../../shared/validators/validators';
+import { ValidatorsService } from '../../../shared/services/validators.service';
+//import * as customValidators from '../../../shared/validators/validators';
 
 @Component({
   templateUrl: './register-page.component.html',
@@ -8,18 +9,20 @@ import * as customValidators from '../../../shared/validators/validators';
 })
 export class RegisterPageComponent {
   public myForm: FormGroup = this.fb.group({
-    name: ["", [Validators.required, Validators.pattern(customValidators.firstNameAndLastnamePattern)]],
-    email: ["", [Validators.required, Validators.pattern(customValidators.emailPattern)]],
-    username: ["", [Validators.required, customValidators.cantBeStrider]],
+    name: ["", [Validators.required, Validators.pattern(this.validatorService.firstNameAndLastnamePattern)]],
+    email: ["", [Validators.required, Validators.pattern(this.validatorService.emailPattern)]],
+    username: ["", [Validators.required, this.validatorService.cantBeStrider]],
     password: ["", [Validators.required, Validators.minLength(6)]],
     passwordConfirmation: ["", [Validators.required]],
   })
 
-  constructor(private fb:FormBuilder){}
+  constructor(
+    private fb:FormBuilder,
+    private validatorService: ValidatorsService,
+  ){}
 
-  isNotValidField( field: string ): boolean | null {
-    return this.myForm.controls[field].errors
-      && this.myForm.controls[field].touched;
+  isNotValidField( field: string ) {
+   this.validatorService.isNotValidField(this.myForm, field)
   }
 
   onSubmit():void {
--- src/app/shared/services/validators.service.ts
+++ src/app/shared/services/validators.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { FormControl, ValidationErrors } from '@angular/forms';
+import { FormControl, FormGroup, ValidationErrors } from '@angular/forms';
 
 @Injectable({
   providedIn: 'root'
@@ -10,6 +10,11 @@ export class ValidatorsService {
   public emailPattern: string = "^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$";
 
   constructor() { }
+
+  public isNotValidField( form: FormGroup, field: string ): boolean | null {
+    return form.controls[field].errors
+      && form.controls[field].touched;
+  }
 
   public cantBeStrider = ( control: FormControl ): ValidationErrors | null => {
 
@@ -23,4 +28,6 @@ export class ValidatorsService {
 
     return null;
   }
+
+
 }
